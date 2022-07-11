ec2022aa84c88df11a744c88304eaac42d635adb
rabeehip <85785396+rabeehip@users.noreply.github.com>
2022-07-11 09:00:16 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5781,6 +5781,8 @@ function list(path) {
   $("#list").html(
     `<div class="mdui-progress"><div class="mdui-progress-indeterminate"></div></div>`
   );
+  $("#readme_md").hide().html("");
+  $("#head_md").hide().html("");
   function successResultCallback(res, path, prevReqParams) {
     $("#list")
       .data("nextPageToken", res.nextPageToken)
@@ -5986,6 +5988,8 @@ function render_search_result_list() {
   $("#list").html(
     `<div class="mdui-progress"><div class="mdui-progress-indeterminate"></div></div>`
   );
+  $("#readme_md").hide().html("");
+  $("#head_md").hide().html("");
   function searchSuccessCallback(res, prevReqParams) {
     $("#list")
       .data("nextPageToken", res.nextPageToken)
